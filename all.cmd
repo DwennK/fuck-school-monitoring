ba640a36b5ba894691e7bd568b33d8a04ccbf1d0
Dwenn Kaufmann <37624106+DwennK@users.noreply.github.com>
2019-02-08 10:56:08 +0100

    Create all.cmd

---
+++ all.cmd
@@ -0,0 +1,15 @@
+:: Désactivation des services Impero sur l'ordinateur
+net stop ImperoGuardianSVC
+net stop ImperoClientSVC
+
+
+::Supression de la clé qui force l'installation du plugin Impero sur chrome
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\ExtensionInstallForcelist /v 1 /f
+
+::Ajout de la clé registre qu permet d'avoir accès au Task Manager intégré à Chrome
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\ /v TaskManagerEndProcessEnabled /d true /f
+
+
+::Modification de la valeur pour pouvoir passer en mode incognito dans chrome (désactivé par défaut par le CPLN)
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\ /v IncognitoModeAvailability /d true /f
+pause
